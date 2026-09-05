b5c552ac0b9e29f79f59b54ff48b3082adb3fe0f
ilies BOUKHATEM <iliesboukhatem@yahoo.fr>
2026-09-05 16:03:22 +0100

    CRUD operations

--- bdd.php
+++ bdd.php
@@ -15,41 +15,78 @@
             $password
         );
 
-        $retour = $canal->query('
-            SELECT * FROM clients WHERE telephone != "" LIMIT 50
-        ');
-
-        // méthode 1 de récupération fetchAall
-        $liste_clients = $retour->fetchAll(PDO::FETCH_OBJ);
-        foreach ($liste_clients as $client) {
-            echo '<li>' . $client->nom . ' - ' . $client->telephone . '</li>';
+        // insertion d'un client - CREAT
+        {
+            // $retour = $canal->query('
+            //     INSERT INTO clients (id_societe, nom, telephone)
+            //     VALUES (1, "mohamed TEST", "0756-89-12-45")
+            // ');
+    
+            // if ($retour->rowCount() == 1) {
+            //     echo 'Le client a bien été inséré !';
+            // } else {
+            //     echo 'Erreur lors de l\'insertion !';
+            // }
         }
 
-        // méthode 2 de récupération boucle while
-        while ( $client = $retour->fetch(PDO::FETCH_OBJ) ) {
-            echo '<li>' . $client->nom . ' - ' . $client->telephone . '</li>';
+        // lecture "affichage" d'un client - READ
+        {
+            // $retour = $canal->query('
+            //     SELECT nom, telephone
+            //     FROM clients
+            //     WHERE nom LIKE "%moh%"
+            //     -- WHERE id = 999999
+            //     -- AND telephone = ""
+            // ');
+
+            // if ( $retour->rowCount() > 0 ) {
+            //     // méthode 1 de récupération fetchAall
+            //     $liste_clients = $retour->fetchAll(PDO::FETCH_OBJ);
+            //     foreach ($liste_clients as $client) {
+            //         echo '<li>' . $client->nom . ' - ' . $client->telephone . '</li>';
+            //     }
+
+            //     // méthode 2 de récupération boucle while
+            //     while ( $client = $retour->fetch(PDO::FETCH_OBJ) ) {
+            //         echo '<li>' . $client->nom . ' - ' . $client->telephone . '</li>';
+            //     }
+            // } else {
+            //     echo 'Aucun résultat trouvé !';
+            // }
         }
 
+        // modifications d'un client - UPDATE
+        {
+            // $retour = $canal->query('
+            //     UPDATE clients
+            //     SET
+            //         nif = "6546512315476",
+            //         nis = "AA/888888888"
+            //     WHERE
+            //         id = 2239
+            // ');
+
+            // if ( $retour->rowCount() == 1 ) {
+            //     echo 'Modification effectuée';
+            // } else {
+            //     echo 'Erreur lors de la modification';
+            // }
+        }
+
+        // modifications d'un client - UPDATE
+        {
+            // $retour = $canal->query('
+            //     DELETE FROM clients
+            //     WHERE id = 2239
+            // ');
+
+            // if ( $retour->rowCount() == 1 ) {
+            //     echo 'Suppression effectuée';
+            // } else {
+            //     echo 'Erreur lors de la suppression';
+            // }
+        }
 
     } catch (PDOException $e) {
-        echo 'Erreur de connexion !';
+        echo $e->getMessage();
     }
-
-    // using mysqli
-    // try
-    // {
-    //     $canal = new mysqli(
-    //         $adresse, 
-    //         $user, 
-    //         $password, 
-    //         $bdd
-    //     );
-
-    //     var_dump($canal);
-
-    // } catch (Exception $e) {
-    //     echo 'Erreur de connexion !';
-    // }
-
-
-    
